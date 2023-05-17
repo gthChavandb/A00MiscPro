761f625bff89460701555c79258cefec061b1872
gthChavandb <dbcIMCpune@gmail.com>
2023-05-17 19:13:33 +0530

    Added New Project P00HelloOOPs

---
+++ P00HelloOOps/P00HelloOOps/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P00HelloOOps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello OOPs !!! P00");
+            Console.ReadLine();
+        }
+    }
+}
